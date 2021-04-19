0cf284bf86b8c976eb6d4cd00b3e049c8c92edb2
WANGJIEKE <tjwang0221@gmail.com>
2021-04-20 02:09:37 +0800

    include comprehension to Conditional Test Logic

--- src/main/java/pytestsmelldetector/ConditionalTestLogicTestSmellDetector.java
+++ src/main/java/pytestsmelldetector/ConditionalTestLogicTestSmellDetector.java
@@ -1,6 +1,5 @@
 package pytestsmelldetector;
 
-import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.intellij.openapi.diagnostic.Logger;
 import com.jetbrains.python.psi.*;
@@ -13,6 +12,7 @@ import java.util.Objects;
 public class ConditionalTestLogicTestSmellDetector extends AbstractTestSmellDetector {
     private static final Logger LOG = Logger.getInstance(ConditionalTestLogicTestSmellDetector.class);
     private final Map<PyFunction, Boolean> testHasConditionalTestLogic = new HashMap<>();
+    private final Map<PyFunction, Boolean> testHasComprehension = new HashMap<>();
     private final ConditionalTestLogicVisitor visitor = new ConditionalTestLogicVisitor();
 
     public ConditionalTestLogicTestSmellDetector(PyClass aTestCase) {
@@ -25,6 +25,7 @@ public class ConditionalTestLogicTestSmellDetector extends AbstractTestSmellDete
         for (PyFunction testMethod : testMethods) {
             currentMethod = testMethod;
             testHasConditionalTestLogic.put(currentMethod, false);
+            testHasComprehension.put(currentMethod, false);
             visitor.visitElement(currentMethod);
         }
         currentMethod = null;
@@ -33,28 +34,28 @@ public class ConditionalTestLogicTestSmellDetector extends AbstractTestSmellDete
     @Override
     public void reset() {
         testHasConditionalTestLogic.clear();
+        testHasComprehension.clear();
     }
 
     @Override
     public void reset(PyClass aTestCase) {
         testCase = aTestCase;
         testHasConditionalTestLogic.clear();
-    }
-
-    public Map<PyFunction, Boolean> getTestHasConditionalTestLogic() {
-        return testHasConditionalTestLogic;
+        testHasComprehension.clear();
     }
 
     @Override
     public boolean hasSmell() {
-        return testHasConditionalTestLogic.containsValue(true);
+        return testHasConditionalTestLogic.containsValue(true) || testHasComprehension.containsValue(true);
     }
 
     @Override
     public JsonObject getSmellDetailJSON() {
         JsonObject jsonObject = templateSmellDetailJSON();
-        JsonArray detailArray = Util.mapToJsonArray(testHasConditionalTestLogic, PyFunction::getName, Objects::toString);
-        jsonObject.add("detail", detailArray);
+        JsonObject detail = new JsonObject();
+        detail.add("testHasConditionalTestLogic", Util.mapToJsonArray(testHasConditionalTestLogic, PyFunction::getName, Objects::toString));
+        detail.add("testHasComprehension", Util.mapToJsonArray(testHasComprehension, PyFunction::getName, Objects::toString));
+        jsonObject.add("detail", detail);
         return jsonObject;
     }
 
@@ -69,6 +70,22 @@ public class ConditionalTestLogicTestSmellDetector extends AbstractTestSmellDete
 
         public void visitPyWhileStatement(PyWhileStatement whileStatement) {
             testHasConditionalTestLogic.put(currentMethod, true);
+        }
+
+        public void visitPyListCompExpression(PyListCompExpression listCompExpression) {
+            testHasComprehension.put(currentMethod, true);
+        }
+
+        public void visitPySetCompExpression(PySetCompExpression setCompExpression) {
+            testHasComprehension.put(currentMethod, true);
+        }
+
+        public void visitPyDictCompExpression(PyDictCompExpression dictCompExpression) {
+            testHasComprehension.put(currentMethod, true);
+        }
+
+        public void visitPyGeneratorExpression(PyGeneratorExpression generatorExpression) {
+            testHasComprehension.put(currentMethod, true);
         }
     }
 }
