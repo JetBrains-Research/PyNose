3b806e661c323ce83e6c881c16d76b0614bed8fc
Oleg Smirnov <sm-oleg1209@yandex.ru>
2021-10-07 17:31:22 +0300

    Add simple initial Default Test inspection

---
+++ plugin/src/main/kotlin/org/jetbrains/research/pynose/plugin/inspections/DefaultTestTestSmellInspection.kt
@@ -0,0 +1,29 @@
+package org.jetbrains.research.pynose.plugin.inspections
+
+import com.intellij.codeInspection.ProblemHighlightType
+import com.intellij.codeInspection.ProblemsHolder
+import com.intellij.openapi.diagnostic.Logger
+import com.intellij.psi.PsiElementVisitor
+import com.jetbrains.python.inspections.PyInspection
+import com.jetbrains.python.psi.PyClass
+import com.jetbrains.python.psi.PyElementVisitor
+import org.jetbrains.research.pynose.core.PyNoseUtils
+
+class DefaultTestTestSmellInspection : PyInspection() {
+    private val LOG = Logger.getInstance(this::class.java)
+
+    override fun buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean): PsiElementVisitor {
+        return object : PyElementVisitor() {
+            override fun visitPyClass(node: PyClass) {
+                super.visitPyClass(node)
+                if (PyNoseUtils.isValidUnittestCase(node) && node.name == "MyTestCase") {
+                    holder.registerProblem(
+                        node,
+                        "Test smell: Default Test in class `${node.name}`",
+                        ProblemHighlightType.WARNING
+                    )
+                }
+            }
+        }
+    }
+}
