a0a4c1801d3748a78dcb4974faa0f2d5335e46e9
OlesyaSub <lesya.sub@mail.ru>
2021-11-03 14:31:55 +0300

    Add magic number inspection

---
+++ plugin/src/main/kotlin/org/jetbrains/research/pynose/plugin/inspections/MagicNumberTestTestSmellInspection.kt
@@ -0,0 +1,78 @@
+package org.jetbrains.research.pynose.plugin.inspections
+
+import com.intellij.codeInspection.ProblemHighlightType
+import com.intellij.codeInspection.ProblemsHolder
+import com.intellij.openapi.diagnostic.Logger
+import com.intellij.psi.PsiElement
+import com.intellij.psi.util.PsiTreeUtil
+import com.jetbrains.python.inspections.PyInspection
+import com.jetbrains.python.psi.*
+import org.jetbrains.research.pynose.core.PyNoseUtils
+import org.jetbrains.research.pynose.plugin.util.TestSmellBundle
+import java.util.*
+
+class MagicNumberTestTestSmellInspection : PyInspection() {
+    private val LOG = Logger.getInstance(MagicNumberTestTestSmellInspection::class.java)
+
+    override fun buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean): PyElementVisitor {
+
+        fun registerMagicNumber(valueParam: PsiElement) {
+            holder.registerProblem(
+                valueParam,
+                TestSmellBundle.message("inspections.magic.number.description"),
+                ProblemHighlightType.WARNING
+            )
+        }
+
+        fun checkParent(element: PsiElement): Boolean {
+            return (PyNoseUtils.isValidUnittestMethod(
+                PsiTreeUtil.getParentOfType(
+                    element,
+                    PyFunction::class.java
+                )
+            )
+                    )
+        }
+
+        return object : PyElementVisitor() {
+
+            override fun visitPyCallExpression(callExpression: PyCallExpression) {
+                super.visitPyCallExpression(callExpression)
+                val child = callExpression.firstChild
+                if (child !is PyReferenceExpression || !PyNoseUtils.isCallAssertMethod(child)
+                    || !checkParent(callExpression)
+                ) {
+                    return
+                }
+                if (Arrays.stream(callExpression.arguments).anyMatch { obj: PyExpression? ->
+                        PyNumericLiteralExpression::class.java.isInstance(obj)
+                                || (PyBinaryExpression::class.java.isInstance(obj)
+                                && Arrays.stream(obj!!.children)
+                            .anyMatch { child ->
+                                PyNumericLiteralExpression::class.java.isInstance(child)
+                            })
+                    }) {
+                    registerMagicNumber(callExpression)
+                }
+            }
+
+            override fun visitPyAssertStatement(assertStatement: PyAssertStatement) {
+                super.visitPyAssertStatement(assertStatement)
+                val assertArgs = assertStatement.arguments
+                if (assertArgs.isEmpty() || !checkParent(assertStatement)) {
+                    return
+                }
+                if (Arrays.stream(assertArgs).anyMatch { obj: PyExpression? ->
+                        PyNumericLiteralExpression::class.java.isInstance(obj)
+                                || (PyBinaryExpression::class.java.isInstance(obj)
+                                && Arrays.stream(obj!!.children)
+                            .anyMatch { child ->
+                                PyNumericLiteralExpression::class.java.isInstance(child)
+                            })
+                    }) {
+                    registerMagicNumber(assertStatement)
+                }
+            }
+        }
+    }
+}
---
+++ plugin/src/test/kotlin/org/jetbrains/research/pynose/plugin/inspections/MagicNumberTestTestSmellInspectionTests.kt
@@ -0,0 +1,62 @@
+package org.jetbrains.research.pynose.plugin.inspections
+
+import com.intellij.lang.annotation.HighlightSeverity
+import org.jetbrains.research.pynose.plugin.util.AbstractTestSmellInspectionTestWithSdk
+import org.jetbrains.research.pynose.plugin.util.TestSmellBundle
+import org.junit.Test
+import org.junit.jupiter.api.BeforeAll
+
+class MagicNumberTestTestSmellInspectionTests: AbstractTestSmellInspectionTestWithSdk() {
+
+    @BeforeAll
+    override fun setUp() {
+        super.setUp()
+        myFixture.enableInspections(MagicNumberTestTestSmellInspection())
+    }
+
+    override fun getTestDataPath(): String {
+        return "src/test/resources/org/jetbrains/research/pynose/plugin/inspections/data/magic_number"
+    }
+
+    @Test
+    fun `test highlighted magic number`() {
+        myFixture.configureByText(
+            "file.py", "import unittest\n" +
+                    "class SomeClass(unittest.TestCase):\n" +
+                    "    def test_something(self):\n" +
+                    "        <warning descr=\"${TestSmellBundle.message("inspections.magic.number.description")}\">assert 1 == 1</warning>\n" +
+                    "        assert \"H\" != \"F\"\n" +
+                    "        <warning descr=\"${TestSmellBundle.message("inspections.magic.number.description")}\">self.assertTrue(2 == 1 + 2)</warning>\n"
+        )
+        myFixture.checkHighlighting()
+    }
+
+    @Test
+    fun `test highlighted combination of numbers and letters`() {
+        myFixture.configureByText(
+            "file.py", "import unittest\n" +
+                    "class SomeClass(unittest.TestCase):\n" +
+                    "    def test_something(self):\n" +
+                    "        <warning descr=\"${TestSmellBundle.message("inspections.magic.number.description")}\">self.assertEqual(\"G\", 2)</warning>"
+        )
+        myFixture.checkHighlighting()
+    }
+
+    @Test
+    fun `test magic number without unittest dependency`() {
+        myFixture.configureByText(
+            "file.py", "import unittest\n" +
+                    "class SomeClass():\n" +
+                    "    def test_something(self):\n" +
+                    "        assert 1 == 1"
+        )
+        val highlightInfos = myFixture.doHighlighting()
+        assertTrue(!highlightInfos.any { it.severity == HighlightSeverity.WARNING })
+    }
+
+    @Test
+    fun `test magic number multiple`() {
+        myFixture.configureByFile("test_magic_number_multiple.py")
+        myFixture.checkHighlighting()
+    }
+}
