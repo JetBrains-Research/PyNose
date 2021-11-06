05e5124ad3365d188103e40d174584994aadbf4c
OlesyaSub <lesya.sub@mail.ru>
2021-11-07 00:03:46 +0300

    Add variables to test file

--- plugin/src/test/resources/org/jetbrains/research/pynose/plugin/inspections/data/suboptimal_assert/test_suboptimal_multiple.py
+++ plugin/src/test/resources/org/jetbrains/research/pynose/plugin/inspections/data/suboptimal_assert/test_suboptimal_multiple.py
@@ -1,6 +1,10 @@
 import unittest
 
 class SomeClass(unittest.TestCase):
+
+    X = 10
+    Y = 20
+
     def test_something(self):
         <warning descr="The assertion is ambiguous and can be replaced by a more specific assertion">self.assertTrue(X != Y)</warning>
 
@@ -11,9 +15,16 @@ class SomeClass(unittest.TestCase):
         <warning descr="The assertion is ambiguous and can be replaced by a more specific assertion">self.assertTrue(X not in Y)</warning>
 
 class OtherClass(unittest.TestCase):
+
+    X = 10
+    Y = 20
+
     def test_something_other(self):
         <warning descr="The assertion is ambiguous and can be replaced by a more specific assertion">self.assertFalse(X in Y)</warning>
 
 class AnotherClass():
+
+    X = True
+
     def test_something_another(self):
         assertIs(X, True)
