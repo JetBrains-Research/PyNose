69b11074363fc39b070644b4b847fb00ac3bce59
OlesiaSub <lesya.sub@mail.ru>
2021-11-29 21:57:15 +0500

    Fix names in test file

--- plugin/src/test/resources/org/jetbrains/research/pynose/plugin/inspections/data/assertion_roulette/pytest/test_roulette_multiple.py
+++ plugin/src/test/resources/org/jetbrains/research/pynose/plugin/inspections/data/assertion_roulette/pytest/test_roulette_multiple.py
@@ -1,6 +1,6 @@
 class TestClass:
 
-    def <warning descr="Test case has multiple non-documented assertions, consider separating or documenting them">test_roulette</warning>(self):
+    def <warning descr="Test case has multiple non-documented assertions, consider separating or documenting them">test_something</warning>(self):
         assert 2 == 2
         assert 4 + 5 == 9
         assert "H" == "J"
@@ -16,13 +16,13 @@ class TestClass:
         assert False
 
 
-def <warning descr="Test case has multiple non-documented assertions, consider separating or documenting them">test_roulette_outside</warning>(self):
+def <warning descr="Test case has multiple non-documented assertions, consider separating or documenting them">test_outside</warning>(self):
     assert 4 + 5 == 9
     assert not 4 + 5 == 4
     assert 2 == 2
     assert "H" == "J"
 
-def test_roulette_with_comments(self):
+def test_outside_with_comments(self):
     assert 2 == 2, "comment"
     assert 4 + 5 == 9
     assert "H" == "J", "comment"
@@ -35,7 +35,7 @@ def do_something(self):
 
 class OtherClass:
 
-    def test_roulette(self):
+    def test_something(self):
         assert True
         assert 2 == 2
         assert "H" != "J"
