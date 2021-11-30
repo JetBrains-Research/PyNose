d544989c5ce71e0c08c0f42704acb42bbf031b81
OlesiaSub <lesya.sub@mail.ru>
2021-11-30 18:54:53 +0500

    Add exception handling quick fix

--- plugin/src/main/kotlin/org/jetbrains/research/pynose/plugin/inspections/pytest/ExceptionHandlingTestSmellPytestInspection.kt
+++ plugin/src/main/kotlin/org/jetbrains/research/pynose/plugin/inspections/pytest/ExceptionHandlingTestSmellPytestInspection.kt
@@ -1,17 +1,12 @@
 package org.jetbrains.research.pynose.plugin.inspections.pytest
 
 import com.intellij.codeInspection.LocalInspectionToolSession
-import com.intellij.codeInspection.ProblemHighlightType
 import com.intellij.codeInspection.ProblemsHolder
 import com.intellij.openapi.diagnostic.Logger
-import com.intellij.openapi.util.TextRange
-import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiElementVisitor
 import com.jetbrains.python.inspections.PyInspection
 import org.jetbrains.research.pynose.plugin.inspections.common.ExceptionHandlingTestSmellVisitor
-import org.jetbrains.research.pynose.plugin.quickfixes.pytest.ExceptionHandlingTestSmellPytestQuickFix
 import org.jetbrains.research.pynose.plugin.startup.PyNoseMode
-import org.jetbrains.research.pynose.plugin.util.TestSmellBundle
 
 class ExceptionHandlingTestSmellPytestInspection : PyInspection() {
     private val LOG = Logger.getInstance(ExceptionHandlingTestSmellPytestInspection::class.java)
@@ -23,17 +18,7 @@ class ExceptionHandlingTestSmellPytestInspection : PyInspection() {
     ): PsiElementVisitor {
 
         return if (PyNoseMode.getPyNosePytestMode()) {
-            object : ExceptionHandlingTestSmellVisitor(holder, session) {
-                override fun registerException(valueParam: PsiElement, offset: Int, textLength: Int) {
-                    holder.registerProblem(
-                        valueParam,
-                        TestSmellBundle.message("inspections.exception.description"),
-                        ProblemHighlightType.WARNING,
-                        TextRange(offset, textLength),
-                        ExceptionHandlingTestSmellPytestQuickFix()
-                    )
-                }
-            }
+            ExceptionHandlingTestSmellVisitor(holder, session)
         } else {
             PsiElementVisitor.EMPTY_VISITOR
         }
