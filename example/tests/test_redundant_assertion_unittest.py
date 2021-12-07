8020a526a61c03d3375e54891e54fe477b4381d0
OlesiaSub <lesya.sub@mail.ru>
2021-12-07 18:42:33 +0500

    Fix example project tests

--- example/tests/test_redundant_assertion_unittest.py
+++ example/tests/test_redundant_assertion_unittest.py
@@ -5,6 +5,10 @@ class SomeClass(unittest.TestCase):
 
     def test_something(self):
         assert 1 == 1
+        self.assertTrue(1)
+        self.assertEqual(2, 3)
+        self.assertTrue()
+        self.assertEqual(1, 1)
 
     def do_something(self):
         assert 2 < 2
@@ -15,8 +19,11 @@ class SomeClass(unittest.TestCase):
 
 class OtherClass(unittest.TestCase):
 
+    def test_somethin_other(self):
+        assert 1 == 1
+
     def test_something_other(self):
-        self.assertTrue(4 >= 4)
+        self.assertTrue(4 <= 4)
 
 
 class AnotherClass:
