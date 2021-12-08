93d6af9267f2ee17fbfab9dabbcb937db6593043
OlesiaSub <lesya.sub@mail.ru>
2021-12-08 16:17:19 +0500

    Complete rebase & add fixes

--- plugin/src/main/kotlin/org/jetbrains/research/pynose/plugin/inspections/pytest/AssertionRouletteTestSmellPytestInspection.kt
+++ plugin/src/main/kotlin/org/jetbrains/research/pynose/plugin/inspections/pytest/AssertionRouletteTestSmellPytestInspection.kt
@@ -5,49 +5,37 @@ import com.intellij.codeInspection.ProblemsHolder
 import com.intellij.openapi.diagnostic.Logger
 import com.intellij.psi.PsiElementVisitor
 import com.intellij.psi.util.PsiTreeUtil
-import com.jetbrains.python.inspections.PyInspection
 import com.jetbrains.python.psi.PyAssertStatement
-import com.jetbrains.python.psi.PyCallExpression
 import com.jetbrains.python.psi.PyFile
-import com.jetbrains.python.psi.PyFunction
+import org.jetbrains.research.pynose.plugin.inspections.AbstractTestSmellInspection
 import org.jetbrains.research.pynose.plugin.inspections.common.AssertionRouletteTestSmellVisitor
-import org.jetbrains.research.pynose.plugin.startup.PyNoseMode
 import org.jetbrains.research.pynose.plugin.util.PytestInspectionsUtils
 
-class AssertionRouletteTestSmellPytestInspection : PyInspection() {
+class AssertionRouletteTestSmellPytestInspection : AbstractTestSmellInspection() {
     private val LOG = Logger.getInstance(AssertionRouletteTestSmellPytestInspection::class.java)
 
-    override fun buildVisitor(
-        holder: ProblemsHolder,
-        isOnTheFly: Boolean,
-        session: LocalInspectionToolSession
-    ): PsiElementVisitor {
-
-        if (PyNoseMode.getPyNosePytestMode()) {
-            return object : AssertionRouletteTestSmellVisitor(holder, session) {
-
-                override fun visitPyFile(node: PyFile) {
-                    super.visitPyFile(node)
-                    if (PytestInspectionsUtils.isValidPytestFile(node)) {
-                        PytestInspectionsUtils.gatherValidPytestMethods(node)
-                            .forEach { testMethod ->
-                                testHasAssertionRoulette[testMethod] = false
-                                PsiTreeUtil
-                                    .collectElements(testMethod) { element -> (element is PyAssertStatement) }
-                                    .forEach { target ->
-                                        processPyAssertStatement(target as PyAssertStatement, testMethod)
-                                    }
-                            }
-                        detectAssertStatementsRoulette()
-                        testHasAssertionRoulette.keys
-                            .filter { key -> testHasAssertionRoulette[key]!! }
-                            .forEach { pyFunction -> registerRoulette(pyFunction.nameIdentifier!!) }
-                        testHasAssertionRoulette.clear()
-                    }
+    override fun buildPytestVisitor(holder: ProblemsHolder, session: LocalInspectionToolSession): PsiElementVisitor {
+        return object : AssertionRouletteTestSmellVisitor(holder, session) {
+            override fun visitPyFile(node: PyFile) {
+                super.visitPyFile(node)
+                if (PytestInspectionsUtils.isValidPytestFile(node)) {
+                    PytestInspectionsUtils.gatherValidPytestMethods(node)
+                        .forEach { testMethod ->
+                            testHasAssertionRoulette[testMethod] = false
+                            PsiTreeUtil
+                                .collectElements(testMethod) { element -> (element is PyAssertStatement) }
+                                .forEach { target ->
+                                    processPyAssertStatement(target as PyAssertStatement, testMethod)
+                                }
+                        }
+                    detectAssertStatementsRoulette()
+                    testHasAssertionRoulette.keys
+                        .filter { key -> testHasAssertionRoulette[key]!! }
+                        .forEach { pyFunction -> registerRoulette(pyFunction.nameIdentifier!!) }
+                    testHasAssertionRoulette.clear()
+                    assertStatementsInTests.clear()
                 }
             }
-        } else {
-            return PsiElementVisitor.EMPTY_VISITOR
         }
     }
 }
--- plugin/src/main/kotlin/org/jetbrains/research/pynose/plugin/inspections/unittest/AssertionRouletteTestSmellUnittestInspection.kt
+++ plugin/src/main/kotlin/org/jetbrains/research/pynose/plugin/inspections/unittest/AssertionRouletteTestSmellUnittestInspection.kt
@@ -5,92 +5,82 @@ import com.intellij.codeInspection.ProblemsHolder
 import com.intellij.openapi.diagnostic.Logger
 import com.intellij.psi.PsiElementVisitor
 import com.intellij.psi.util.PsiTreeUtil
-import com.jetbrains.python.inspections.PyInspection
 import com.jetbrains.python.psi.*
+import org.jetbrains.research.pynose.plugin.inspections.AbstractTestSmellInspection
 import org.jetbrains.research.pynose.plugin.inspections.common.AssertionRouletteTestSmellVisitor
-import org.jetbrains.research.pynose.plugin.startup.PyNoseMode
 import org.jetbrains.research.pynose.plugin.util.UnittestInspectionsUtils
 
-class AssertionRouletteTestSmellUnittestInspection : PyInspection() {
+class AssertionRouletteTestSmellUnittestInspection : AbstractTestSmellInspection() {
     private val LOG = Logger.getInstance(AssertionRouletteTestSmellUnittestInspection::class.java)
     private val assertionCallsInTests: MutableMap<PyFunction, MutableSet<PyCallExpression>> = mutableMapOf()
 
-    override fun buildVisitor(
-        holder: ProblemsHolder,
-        isOnTheFly: Boolean,
-        session: LocalInspectionToolSession
-    ): PsiElementVisitor {
-
-        if (PyNoseMode.getPyNoseUnittestMode()) {
-            return object : AssertionRouletteTestSmellVisitor(holder, session) {
-                override fun visitPyClass(pyClass: PyClass) {
-                    if (UnittestInspectionsUtils.isValidUnittestCase(pyClass)) {
-                        testHasAssertionRoulette.clear()
-                        UnittestInspectionsUtils.gatherUnittestTestMethods(pyClass).forEach { testMethod ->
-                            testHasAssertionRoulette[testMethod] = false
-                            PsiTreeUtil
-                                .collectElements(testMethod) { element -> (element is PyCallExpression) }
-                                .forEach { target -> processPyCallExpression(target as PyCallExpression, testMethod) }
-                            PsiTreeUtil
-                                .collectElements(testMethod) { element -> (element is PyAssertStatement) }
-                                .forEach { target -> processPyAssertStatement(target as PyAssertStatement, testMethod) }
-                        }
-                        detectAssertCallsRoulette()
-                        detectAssertStatementsRoulette()
-                        getRoulette()
-                        testHasAssertionRoulette.keys
-                            .filter { key -> testHasAssertionRoulette[key]!! }
-                            .forEach { pyFunction -> registerRoulette(pyFunction.nameIdentifier!!) }
-                        testHasAssertionRoulette.clear()
+    override fun buildUnittestVisitor(holder: ProblemsHolder, session: LocalInspectionToolSession): PsiElementVisitor {
+        return object : AssertionRouletteTestSmellVisitor(holder, session) {
+            override fun visitPyClass(pyClass: PyClass) {
+                if (UnittestInspectionsUtils.isValidUnittestCase(pyClass)) {
+                    testHasAssertionRoulette.clear()
+                    UnittestInspectionsUtils.gatherUnittestTestMethods(pyClass).forEach { testMethod ->
+                        testHasAssertionRoulette[testMethod] = false
+                        PsiTreeUtil
+                            .collectElements(testMethod) { element -> (element is PyCallExpression) }
+                            .forEach { target -> processPyCallExpression(target as PyCallExpression, testMethod) }
+                        PsiTreeUtil
+                            .collectElements(testMethod) { element -> (element is PyAssertStatement) }
+                            .forEach { target -> processPyAssertStatement(target as PyAssertStatement, testMethod) }
                     }
+                    detectAssertCallsRoulette()
+                    detectAssertStatementsRoulette()
+                    getRoulette()
+                    testHasAssertionRoulette.keys
+                        .filter { key -> testHasAssertionRoulette[key]!! }
+                        .forEach { pyFunction -> registerRoulette(pyFunction.nameIdentifier!!) }
+                    testHasAssertionRoulette.clear()
                 }
+            }
 
-                private fun detectAssertCallsRoulette() {
-                    for (testMethod in assertionCallsInTests.keys) {
-                        val calls: MutableSet<PyCallExpression>? = assertionCallsInTests[testMethod]
-                        if (calls!!.size < 2) {
+            private fun detectAssertCallsRoulette() {
+                for (testMethod in assertionCallsInTests.keys) {
+                    val calls: MutableSet<PyCallExpression>? = assertionCallsInTests[testMethod]
+                    if (calls!!.size < 2) {
+                        continue
+                    }
+                    for (call in calls) {
+                        val argumentList = call.argumentList
+                        if (argumentList == null) {
+                            LOG.warn("assertion with no argument")
                             continue
                         }
-                        for (call in calls) {
-                            val argumentList = call.argumentList
-                            if (argumentList == null) {
-                                LOG.warn("assertion with no argument")
-                                continue
-                            }
-                            if (argumentList.getKeywordArgument("msg") != null) {
-                                continue
-                            }
-                            if (UnittestInspectionsUtils.ASSERT_METHOD_TWO_PARAMS
-                                    .contains((call.callee as PyReferenceExpression).name) &&
-                                argumentList.arguments.size < 3
-                            ) {
-                                testHasAssertionRoulette.replace(testMethod, true)
-                            } else if (UnittestInspectionsUtils.ASSERT_METHOD_ONE_PARAM
-                                    .containsKey((call.callee as PyReferenceExpression).name) &&
-                                argumentList.arguments.size < 2
-                            ) {
-                                testHasAssertionRoulette.replace(testMethod, true)
-                            }
+                        if (argumentList.getKeywordArgument("msg") != null) {
+                            continue
                         }
-                    }
-                }
-
-                private fun getRoulette() {
-                    for (testMethod in assertStatementsInTests.keys) {
-                        if (assertStatementsInTests[testMethod]?.size == 1 && assertionCallsInTests[testMethod]?.size == 1) {
+                        if (UnittestInspectionsUtils.ASSERT_METHOD_TWO_PARAMS
+                                .contains((call.callee as PyReferenceExpression).name) &&
+                            argumentList.arguments.size < 3
+                        ) {
+                            testHasAssertionRoulette.replace(testMethod, true)
+                        } else if (UnittestInspectionsUtils.ASSERT_METHOD_ONE_PARAM
+                                .containsKey((call.callee as PyReferenceExpression).name) &&
+                            argumentList.arguments.size < 2
+                        ) {
                             testHasAssertionRoulette.replace(testMethod, true)
                         }
                     }
                 }
+            }
 
-                private fun processPyCallExpression(callExpression: PyCallExpression, testMethod: PyFunction) {
-                    if (callExpression.callee is PyReferenceExpression) {
-                        assertionCallsInTests.getOrPut(testMethod) { mutableSetOf() }.add(callExpression)
+            private fun getRoulette() {
+                for (testMethod in assertStatementsInTests.keys) {
+                    if (assertStatementsInTests[testMethod]?.size == 1 && assertionCallsInTests[testMethod]?.size == 1) {
+                        testHasAssertionRoulette.replace(testMethod, true)
                     }
                 }
             }
-        } else {
-            return PsiElementVisitor.EMPTY_VISITOR
+
+            private fun processPyCallExpression(callExpression: PyCallExpression, testMethod: PyFunction) {
+                if (callExpression.callee is PyReferenceExpression) {
+                    assertionCallsInTests.getOrPut(testMethod) { mutableSetOf() }.add(callExpression)
+                }
+            }
         }
     }
 }
