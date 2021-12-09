8a1410629b3db8423e2960191f1daf5d9e46ef79
OlesiaSub <lesya.sub@mail.ru>
2021-12-10 03:54:49 +0500

    Return example file to its initial state

--- example/tests/test_exception_unittest.py
+++ example/tests/test_exception_unittest.py
@@ -3,10 +3,33 @@ import unittest
 
 class SomeClass(unittest.TestCase):
 
-    def setUp(self):
-        self.x = 10
+    def test_something(self):
+        with self.assertRaises(ValueError):
+            x = 3
+        with self.assertRaises(ValueError):
+            x = 3
+        try:
+            x = 3
+        except ValueError:
+            print("Try again...")
 
-    def test_division(self):
-        with self.assertRaises(ZeroDivisionError):
-            for i in range(5):
-                print(self.x / i)
+    def do_something(self):
+        raise NameError('HiThere')
+
+    def test_something_else(self):
+        raise NameError('HiThere')
+
+
+class OtherClass(unittest.TestCase):
+
+    def test_something_other(self):
+        try:
+            x = 5
+        except ValueError:
+            print("Fail!")
+
+
+class AnotherClass:
+
+    def test_something_another(self):
+        raise NameError('HiThere')
