f5a0cafd811921d85e8598ea57fee57ecda03301
OlesiaSub <lesya.sub@mail.ru>
2021-12-13 06:42:20 +0500

    Add ignored numbers

--- plugin/src/test/resources/org/jetbrains/research/pynose/plugin/inspections/data/magic_number/pytest/test_magic_number_multiple.py
+++ plugin/src/test/resources/org/jetbrains/research/pynose/plugin/inspections/data/magic_number/pytest/test_magic_number_multiple.py
@@ -4,21 +4,21 @@ import unittest
 class TestClass:
 
     def test_something(self):
-        <weak_warning descr="Consider replacing numeric literals with more descriptive constants or variables">assert 2 == 2</weak_warning>
+        <weak_warning descr="Consider replacing numeric literals with more descriptive constants or variables">assert 24 == 2</weak_warning>
 
     def do_something(self):
-        assert 1 == 1
+        assert 1 == 15
 
     def test_something_else(self):
-        <weak_warning descr="Consider replacing numeric literals with more descriptive constants or variables">assert 1 == 1</weak_warning>
+        assert 1 == 5
 
 
 
 def test_something_other(self):
-    <weak_warning descr="Consider replacing numeric literals with more descriptive constants or variables">assert 5 != 6</weak_warning>
+    <weak_warning descr="Consider replacing numeric literals with more descriptive constants or variables">assert 56 != 6</weak_warning>
 
 
 class AnotherClass:
 
     def test_something_another(self):
-        assert 5 == 5
+        assert 5 == 53
--- plugin/src/test/resources/org/jetbrains/research/pynose/plugin/inspections/data/magic_number/unittest/test_magic_number_multiple.py
+++ plugin/src/test/resources/org/jetbrains/research/pynose/plugin/inspections/data/magic_number/unittest/test_magic_number_multiple.py
@@ -4,22 +4,22 @@ import unittest
 class SomeClass(unittest.TestCase):
 
     def test_something(self):
-        <weak_warning descr="Consider replacing numeric literals with more descriptive constants or variables">assert 2 == 2</weak_warning>
+        <weak_warning descr="Consider replacing numeric literals with more descriptive constants or variables">assert 24 == 2</weak_warning>
 
     def do_something(self):
-        self.assertEqual(1, 1)
+        self.assertEqual(14, 1)
 
     def test_something_else(self):
-        <weak_warning descr="Consider replacing numeric literals with more descriptive constants or variables">self.assertEqual(1, 1)</weak_warning>
+        self.assertEqual(3, 1)
 
 
 class OtherClass(unittest.TestCase):
 
     def test_something_other(self):
-        <weak_warning descr="Consider replacing numeric literals with more descriptive constants or variables">assert 5 != 6</weak_warning>
+        <weak_warning descr="Consider replacing numeric literals with more descriptive constants or variables">assert 50 != 6</weak_warning>
 
 
 class AnotherClass:
 
     def test_something_another(self):
-        assert 5 == 5
+        assert 5 == 54
