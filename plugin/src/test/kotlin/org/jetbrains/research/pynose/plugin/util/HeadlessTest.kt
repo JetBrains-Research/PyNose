bd0309a3ca730a052dc482fbe33fd9d20ee63911
OlesiaSub <lesya.sub@mail.ru>
2021-12-21 16:09:14 +0500

    Fix issue with unittests

--- plugin/src/test/kotlin/org/jetbrains/research/pynose/plugin/util/HeadlessTest.kt
+++ plugin/src/test/kotlin/org/jetbrains/research/pynose/plugin/util/HeadlessTest.kt
@@ -6,31 +6,46 @@ import com.intellij.codeInspection.ProblemsHolder
 import com.intellij.ide.impl.ProjectUtil
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.command.WriteCommandAction
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.projectRoots.ProjectJdkTable
+import com.intellij.openapi.projectRoots.Sdk
+import com.intellij.openapi.roots.ProjectRootManager
 import com.intellij.psi.search.FilenameIndex
 import com.intellij.psi.search.GlobalSearchScope
-import org.jetbrains.research.pynose.plugin.inspections.pytest.DuplicateAssertionTestSmellPytestInspection
+import com.intellij.psi.util.PsiTreeUtil
+import com.intellij.testFramework.fixtures.BasePlatformTestCase
+import com.jetbrains.python.psi.PyAssertStatement
+import com.jetbrains.python.psi.PyCallExpression
+import org.jetbrains.research.pluginUtilities.sdk.PythonMockSdk
+import org.jetbrains.research.pluginUtilities.sdk.SdkConfigurer
+import org.jetbrains.research.pynose.plugin.inspections.unittest.MagicNumberTestTestSmellUnittestInspection
 import org.junit.Test
-import java.io.File
 
-class HeadlessTest : AbstractTestSmellInspectionTestWithSdk() {
-    override fun getTestDataPath(): String {
-        return "path"
+class HeadlessTest : BasePlatformTestCase() {
+
+    // set yours
+    private val projectRoot = "C:\\Users\\Olesya\\PycharmProjects\\PyNoseTest"
+    private lateinit var sdk: Sdk
+
+    private fun setupSdk(project : Project) {
+        val projectManager = ProjectRootManager.getInstance(project)
+        sdk = PythonMockSdk(projectRoot).create("3.8")
+        val sdkConfigurer = SdkConfigurer(project, projectManager)
+        sdkConfigurer.setProjectSdk(sdk)
     }
 
     @Test
     fun `test headless`() {
-
-        // set yours
-        val projectRoot = File("C:\\Users\\Olesya\\PycharmProjects\\PyNoseTest")
         ApplicationManager.getApplication().invokeAndWait {
 
             // throws an exception (smth about a pop-up message)
 //            val project = ProjectUtil.openOrImport(projectRoot.toPath())
 
-            val project = ProjectUtil.openProject(projectRoot.toString(), null, true)
-                ?: return@invokeAndWait
+            val project = ProjectUtil.openProject(projectRoot, null, true) ?: return@invokeAndWait
+            setupSdk(project)
             val inspectionManager = InspectionManager.getInstance(project)
             WriteCommandAction.runWriteCommandAction(project) {
+                // if I add new files to the project it won't find them
                 FilenameIndex.getAllFilesByExt(project, "py", GlobalSearchScope.projectScope(project))
                     .filter { vFile ->
                         vFile.name.startsWith("test_") || vFile.name.endsWith("_test.py")
@@ -39,24 +54,25 @@ class HeadlessTest : AbstractTestSmellInspectionTestWithSdk() {
                         FilenameIndex.getFilesByName(project, vFile.name, GlobalSearchScope.projectScope(project))
                     }.forEach { psiFileList ->
                         psiFileList.forEach { psiFile ->
-                            println(psiFile.name)
                             val holder = ProblemsHolder(inspectionManager, psiFile, false)
                             val session = LocalInspectionToolSession(psiFile, 0, psiFile.textLength)
                             val inspectionVisitor =
-                                DuplicateAssertionTestSmellPytestInspection().buildVisitor(holder, false, session)
+                                MagicNumberTestTestSmellUnittestInspection().buildVisitor(holder, false, session)
+                            
+                            PsiTreeUtil.findChildrenOfType(psiFile, PyAssertStatement::class.java).forEach {
+                                it.accept(inspectionVisitor)
+                            }
+                            PsiTreeUtil.findChildrenOfType(psiFile, PyCallExpression::class.java).forEach {
+                                it.accept(inspectionVisitor)
+                            }
 
-                            // for other types of visitors
-//                            PsiTreeUtil.findChildrenOfType(psiFile, PyFile::class.java)
-//                                .forEach {
-//                                    it.accept(inspectionVisitor)
-//                                }
-
-                            // currently visits only psi files
-                            psiFile.accept(inspectionVisitor)
-                            println(holder.results)
+                            println("In ${psiFile.name} ${holder.results.size} magic number test smells were found")
                         }
                     }
             }
+        }
+        ApplicationManager.getApplication().runWriteAction {
+            ProjectJdkTable.getInstance().removeJdk(sdk)
         }
     }
 }
